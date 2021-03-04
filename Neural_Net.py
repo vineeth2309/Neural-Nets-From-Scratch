b4df0a54fdd92f27fd403c6aa92a93e78ae79bd1
Vineeth S Subramanyam <vineeth.subramanyam@fluxauto.xyz>
2021-03-04 09:38:06 +0530

    Updated code to use tensor class. Completed backward pass for layers, activations and loss functions

--- Neural_Net.py
+++ Neural_Net.py
@@ -11,24 +11,31 @@ class tensor:
 		self.shape = x.shape
 		self.grad = None
 		
-class Linear:
-	def __init__(self, input_shape, output_shape, activation="sigmoid", leaky_slope=0.01):
-		self.input_dims = input_shape
-		self.output_dims = output_shape
-		self.weights = np.random.randn(self.input_dims, self.output_dims)
-		self.bias = np.zeros((self.output_dims, 1))
-		self.activation = Activation(name=activation)
-		self.dlw = np.zeros((self.input_dims, self.output_dims))
-		self.dlb = np.zeros((self.output_dims, 1))
-		self.dl = None
+class loss:
+	def __init__(self, name="cross_entropy"):
+		self.name = name
 	
-	def __call__(self, x):
-		self.backward(x)
-		return tensor(self.activation(np.matmul(x.data, self.weights) + self.bias.T))
+	def __call__(self, yhat , y):
+		""" yhat-> model prediction, y -> label"""
+		loss = tensor(np.zeros_like(yhat.data)) 
+		if self.name == "cross_entropy":
+			loss.data = np.mean(np.sum(-((y.data * np.log(yhat.data)) + ((1-y.data) * np.log(1 - yhat.data))), axis=1))
+			if yhat.requires_grad:
+				loss.grad = (yhat.data - y.data) / (yhat.data * (1 - yhat.data))  
+				yhat.grad = (yhat.data - y.data) / (yhat.data * (1 - yhat.data))  
 
-	def backward(self, x):
-		if x.grad == None:
-			print("HERE")
+		elif self.name == "mse":
+			loss.data = np.mean((np.sum((yhat.data - y.data)**2, axis=1)) / y.data.shape[1])
+			if yhat.requires_grad:
+				loss.grad = yhat.data - y.data
+				yhat.grad = yhat.data - y.data
+
+		elif self.name == "rmse":
+			loss.data = np.mean((np.sum((yhat.data - y.data)**2, axis=1) ** 0.5) / y.data.shape[1])
+			if yhat.requires_grad:
+				loss.grad = (yhat.data - y.data)**0.5
+				yhat.grad = (yhat.data - y.data)**0.5
+		return loss, yhat
 
 class Activation:
 	def __init__(self, name='sigmoid', leaky_slope=0.01):
@@ -44,15 +51,55 @@ class Activation:
 			elif self.name == "tanh":
 				return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
 			elif self.name == "leaky_relu":
-				return np.maximum(x, -self.leaky_slope * x)
+				return np.maximum(x, self.leaky_slope * x)
 			elif self.name =="softmax":
 				return np.exp(x) / sum(np.exp(x)[0])
 		else:
 			return None
 	
 	def backward(self, x):
-		pass
+		if self.name == "sigmoid":
+			x.grad = x.grad * (x.data * (1 - x.data))
+			return x 
+		elif self.name == "relu":
+			x.grad = x.grad * (x.data > 0) * 1
+			return x
+		elif self.name == "leaky_relu":
+			dx = np.ones_like(x.data)
+			dx[x.data < 0] = self.leaky_slope
+			x.grad = x.grad * dx
+			return x
+		elif self.name =="tanh":
+			x.grad = x.grad * (1 - (x.data**2))
+			return x
+		elif self.name == "softmax":
+			x.grad = x.grad * (x.data * (1 - x.data))
+			return x
 
+class Linear:
+	def __init__(self, input_shape, output_shape, activation="sigmoid", leaky_slope=0.01):
+		self.input_dims = input_shape
+		self.output_dims = output_shape
+		self.weights = np.random.randn(self.input_dims, self.output_dims)
+		self.bias = np.zeros((1, self.output_dims))
+		self.activation = Activation(name=activation)
+		self.dlw = np.zeros((self.input_dims, self.output_dims))
+		self.dlb = np.zeros((self.output_dims, 1))
+		self.data = None
+		self.h_past = None
+	
+	def __call__(self, x):
+		self.data = x
+		return tensor(self.activation(np.matmul(x.data, self.weights) + self.bias))
+
+	def backward(self, x):
+		dla = self.activation.backward(x)
+		self.dlw = np.matmul(self.data.data.T, dla.grad)
+		self.dlb = dla.grad
+		x.grad = np.matmul(dla.grad, self.weights.T)
+		x.data = self.data.data
+		return x
+		
 class Neural_Network:
 	def __init__(self):
 		self.net = {}
@@ -67,13 +114,26 @@ class Neural_Network:
 		x = self.layer3(x)
 		x = self.layer4(x)
 		return x
+	
+	def backward(self, yhat):
+		x = self.layer4.backward(yhat)
+		x = self.layer3.backward(x)
+		x = self.layer2.backward(x)
+		x = self.layer1.backward(x)
 
 class Main:
 	def __init__(self):
 		self.X = tensor(np.ones((10, 2)), requires_grad=True)
+		self.Y = tensor(np.zeros((10,2)), requires_grad=False)		
 		self.net = Neural_Network()
-		print(self.net.forward(self.X).data)
-		
+		self.loss = loss("cross_entropy")
+		self.forward()
+	
+	def forward(self):
+		out = self.net.forward(self.X)
+		loss, out = self.loss(out, self.Y)
+		self.net.backward(out)
+
 
 if __name__ == "__main__":
 	Main()
